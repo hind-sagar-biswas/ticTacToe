bb405eb5512ed4e8472176c457513c4458924941
Hind Sagar Biswas <hindsbhk@gmail.com>
2022-12-01 19:33:54 +0600

    switched to nodejs

---
+++ server.js
@@ -0,0 +1,25 @@
+const DEBUG = true;
+
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+
+// Static
+app.use(express.static(path.join(__dirname, "public")));
+
+// Client connection
+io.on("connection", (socket) => {
+	console.log(`A new connection: ${socket.id}`);
+});
+
+const port = 3000;
+
+server.listen(port, () =>
+	console.log(`Game server listening on http://localhost:${port}/`)
+);
